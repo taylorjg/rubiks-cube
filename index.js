dfd99348d18ff21034e4653a77d1ef67c0c12fec
Jonathan Taylor <jont@cakesolutions.net>
2018-02-23 09:28:00 +0000

    Begin the idea of reordering the colours string when rotating

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ import math, { matrix } from 'mathjs';
 
 const solvedCube = [
   // top layer
-  { id: 1, x: 0, y: 2, z: 0, colours: "BRY---", orientation: "U" },
-  { id: 2, x: 1, y: 2, z: 0, colours: "B-Y---", orientation: "U" },
-  { id: 3, x: 2, y: 2, z: 0, colours: "B-YO--", orientation: "U" },
-  { id: 4, x: 0, y: 2, z: 1, colours: "BR----", orientation: "U" },
-  { id: 5, x: 1, y: 2, z: 1, colours: "B-----", orientation: "U" },
-  { id: 6, x: 2, y: 2, z: 1, colours: "B--O--", orientation: "U" },
-  { id: 7, x: 0, y: 2, z: 2, colours: "BR--W-", orientation: "U" },
-  { id: 8, x: 1, y: 2, z: 2, colours: "B---W-", orientation: "U" },
-  { id: 9, x: 2, y: 2, z: 2, colours: "B--OW-", orientation: "U" },
+  { id: 1, x: 0, y: 2, z: 0, colours: "BRY---" },
+  { id: 2, x: 1, y: 2, z: 0, colours: "B-Y---" },
+  { id: 3, x: 2, y: 2, z: 0, colours: "B-YO--" },
+  { id: 4, x: 0, y: 2, z: 1, colours: "BR----" },
+  { id: 5, x: 1, y: 2, z: 1, colours: "B-----" },
+  { id: 6, x: 2, y: 2, z: 1, colours: "B--O--" },
+  { id: 7, x: 0, y: 2, z: 2, colours: "BR--W-" },
+  { id: 8, x: 1, y: 2, z: 2, colours: "B---W-" },
+  { id: 9, x: 2, y: 2, z: 2, colours: "B--OW-" },
 
   // middle layer
-  { id: 10, x: 0, y: 1, z: 0, colours: "-RY---", orientation: "U" },
-  { id: 11, x: 1, y: 1, z: 0, colours: "--Y---", orientation: "U" },
-  { id: 12, x: 2, y: 1, z: 0, colours: "--YO--", orientation: "U" },
-  { id: 13, x: 0, y: 1, z: 1, colours: "-R----", orientation: "U" },
-  { id: 14, x: 1, y: 1, z: 1, colours: "------", orientation: "U" },
-  { id: 15, x: 2, y: 1, z: 1, colours: "---O--", orientation: "U" },
-  { id: 16, x: 0, y: 1, z: 2, colours: "-R--W-", orientation: "U" },
-  { id: 17, x: 1, y: 1, z: 2, colours: "----W-", orientation: "U" },
-  { id: 18, x: 2, y: 1, z: 2, colours: "---OW-", orientation: "U" },
+  { id: 10, x: 0, y: 1, z: 0, colours: "-RY---" },
+  { id: 11, x: 1, y: 1, z: 0, colours: "--Y---" },
+  { id: 12, x: 2, y: 1, z: 0, colours: "--YO--" },
+  { id: 13, x: 0, y: 1, z: 1, colours: "-R----" },
+  { id: 14, x: 1, y: 1, z: 1, colours: "------" },
+  { id: 15, x: 2, y: 1, z: 1, colours: "---O--" },
+  { id: 16, x: 0, y: 1, z: 2, colours: "-R--W-" },
+  { id: 17, x: 1, y: 1, z: 2, colours: "----W-" },
+  { id: 18, x: 2, y: 1, z: 2, colours: "---OW-" },
 
   // bottom layer
-  { id: 19, x: 0, y: 0, z: 0, colours: "-RY--G", orientation: "U" },
-  { id: 20, x: 1, y: 0, z: 0, colours: "--Y--G", orientation: "U" },
-  { id: 21, x: 2, y: 0, z: 0, colours: "--YO-G", orientation: "U" },
-  { id: 22, x: 0, y: 0, z: 1, colours: "-R---G", orientation: "U" },
-  { id: 23, x: 1, y: 0, z: 1, colours: "-----G", orientation: "U" },
-  { id: 24, x: 2, y: 0, z: 1, colours: "---O-G", orientation: "U" },
-  { id: 25, x: 0, y: 0, z: 2, colours: "-R--WG", orientation: "U" },
-  { id: 26, x: 1, y: 0, z: 2, colours: "----WG", orientation: "U" },
-  { id: 27, x: 2, y: 0, z: 2, colours: "---OWG", orientation: "U" }
+  { id: 19, x: 0, y: 0, z: 0, colours: "-RY--G" },
+  { id: 20, x: 1, y: 0, z: 0, colours: "--Y--G" },
+  { id: 21, x: 2, y: 0, z: 0, colours: "--YO-G" },
+  { id: 22, x: 0, y: 0, z: 1, colours: "-R---G" },
+  { id: 23, x: 1, y: 0, z: 1, colours: "-----G" },
+  { id: 24, x: 2, y: 0, z: 1, colours: "---O-G" },
+  { id: 25, x: 0, y: 0, z: 2, colours: "-R--WG" },
+  { id: 26, x: 1, y: 0, z: 2, colours: "----WG" },
+  { id: 27, x: 2, y: 0, z: 2, colours: "---OWG" }
 ];
 
 const topCoordsList = [
@@ -107,6 +107,21 @@ const bottomCoordsList = [
   [2, 0, 2]
 ];
 
+// TODO: we need three more coords lists:
+// - yawMiddleCoordsList
+// - pitchMiddleCoordsList
+// - rollMiddleCoordsList
+
+const RY90 = matrix([
+  [0, 0, -1],
+  [0, 1, 0,],
+  [1, 0, 0]
+]);
+
+const X_AXIS = "X";
+const Y_AXIS = "Y";
+const Z_AXIS = "Z";
+
 const pieceHasCoords = coords => piece =>
   piece.x === coords[0] &&
   piece.y === coords[1] &&
@@ -140,21 +155,19 @@ const getBackFace = cube =>
 const getBottomFace = cube =>
   getPieces(cube, bottomCoordsList).map(piece => piece.colours[5]);
 
-const ry90 = matrix([
-  [0, 0, -1],
-  [0, 1, 0,],
-  [1, 0, 0]
-]);
-
 const axisToTranslation = axis => {
   switch (axis) {
-    case "X": return matrix([0, -1, -1]);
-    case "Y": return matrix([-1, 0, -1]);
-    case "Z": return matrix([-1, -1, 0]);
+    case X_AXIS: return matrix([0, -1, -1]);
+    case Y_AXIS: return matrix([-1, 0, -1]);
+    case Z_AXIS: return matrix([-1, -1, 0]);
   }
 };
 
-const transform = (piece, r, axis) => {
+const reorderColours = (colours, newColoursOrder) =>
+    // TODO: reorder piece.colours using newColoursOrder
+    colours;
+
+const transform = (r, axis, newColoursOrder) => piece => {
   const p1 = axisToTranslation(axis);
   const p2 = p1.map(x => -x);
   const p = math.add(math.multiply(p1, r), p2);
@@ -163,19 +176,22 @@ const transform = (piece, r, axis) => {
     .subset(math.index(3, [0, 1, 2]), p);
   const v1 = matrix([piece.x, piece.y, piece.z, 1]);
   const v2 = math.multiply(v1, m);
-  const v2arr = v2.toArray();
   return {
     id: piece.id,
-    x: v2arr[0],
-    y: v2arr[1],
-    z: v2arr[2],
-    colours: piece.colours,
-    orientation: piece.orientation
+    x: v2.get([0]),
+    y: v2.get([1]),
+    z: v2.get([2]),
+    colours: reorderColours(piece.colours, newColoursOrder)
   };
 };
 
+const transformPieces = (cube, coordsList, transform) =>
+  cube.map(piece => isPieceInCoordsList(coordsList, piece)
+    ? transform(piece)
+    : piece);
+
 const yawTop90 = cube =>
-  cube.map(piece => isPieceInCoordsList(topCoordsList, piece) ? transform(piece, ry90, "Y") : piece);
+  transformPieces(cube, topCoordsList, transform(RY90, Y_AXIS, "023415"));
 
 const yawMiddle90 = cube => {
   return cube;
