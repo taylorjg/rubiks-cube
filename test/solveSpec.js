e19913d2298ce37674051135988a6c14d4d664bf
Jonathan Taylor <jont@cakesolutions.net>
2018-02-24 17:33:28 +0000

    Add shuffleCube test helper and another unit test

--- test/solveSpec.js
+++ test/solveSpec.js
@@ -4,19 +4,30 @@ import * as R from '../index';
 
 describe("solve tests", () => {
 
-  it("already solved", () => {
-    const result = solve(solvedCube);
+  const shuffleCube = moves =>
+    moves.reduce((cube, move) => move(cube), solvedCube);
+
+  it("zero moves", () => {
+    const shuffledCube = shuffleCube([]);
+    const result = solve(shuffledCube);
     expect(result).not.to.be.null;
     expect(result.cube).to.deep.equal(solvedCube);
     expect(result.parent).to.be.null;
   });
 
   it("one move", () => {
-    const shuffledCube = R.yawTop90(solvedCube);
+    const shuffledCube = shuffleCube([R.yawTop90]);
     const result = solve(shuffledCube);
     expect(result).not.to.be.null;
     expect(result.cube).to.deep.equal(solvedCube);
     expect(result.parent).not.to.be.null;
     expect(result.move).to.equal(R.yawTop270);
+  });
+
+  it("two moves", () => {
+    const shuffledCube = shuffleCube([R.yawTop90, R.rollFront180]);
+    const result = solve(shuffledCube);
+    expect(result).not.to.be.null;
+    expect(result.parent).not.to.be.null;
   });
 });
