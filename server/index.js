97afa087861949053c532916438eefa882a85fbf
Jonathan Taylor <jont@cakesolutions.net>
2018-02-25 09:41:59 +0000

    Add client and server and webpack etc

---
+++ server/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const path = require('path');
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+const publicFolder = path.join(__dirname, 'public');
+
+const app = express();
+app.use('/', express.static(publicFolder));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const packageJson = require('./package.json');
+
+const serverPublic = path.join(__dirname, 'server', 'public');
+
+module.exports = {
+    entry: [
+        './solving/index.js',
+        './client/index.js'
+    ],
+    output: {
+        path: serverPublic,
+        filename: 'bundle.js',
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { context: './client', from: '*.html' },
+            { context: './client', from: '*.css' }
+        ]),
+        new HtmlWebpackPlugin({
+            template: './client/index.html',
+            version: packageJson.version
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: serverPublic
+    }
+};
