7a4eb7c88287dd38e047f45961320434e745eed6
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-03-10 22:52:19 +0000

    Register service worker

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister()
+// serviceWorkerRegistration.unregister()
+serviceWorkerRegistration.register()
 
 threeAppActions.init()
